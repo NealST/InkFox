8dbcb13d17282e0fe62826b1e3a55d5171c2aa24
NealST <m13710224694@163.com>
2025-01-12 22:14:09 +0800

    fix: add format

--- src/components/editor/content/index.tsx
+++ src/components/editor/content/index.tsx
@@ -1,32 +1,32 @@
-import Title from './title';
-import cn from 'classnames';
+import Title from "./title";
+import cn from "classnames";
 import {
-    useContentState,
-    type IContentState,
-  } from "./controllers/datasource-state";
+  useContentState,
+  type IContentState,
+} from "./controllers/datasource-state";
 import blockMap from "./blocks";
-import styles from './index.module.css';
-import 'github-markdown-css/github-markdown.css';
+import styles from "./index.module.css";
+import "github-markdown-css/github-markdown.css";
 
 type BlockKey = keyof typeof blockMap;
 
-const Content = function() {
-  const dataSource = useContentState((state: IContentState) => state.dataSource);
+const Content = function () {
+  const dataSource = useContentState(
+    (state: IContentState) => state.dataSource
+  );
 
   return (
     <div className={styles.content}>
       <Title />
-      <div className={cn(styles.content_body, 'markdown-body')}>
-        {
-          dataSource.map((item, index) => {
-            const { name, id } = item;
-            const Com = blockMap[name as BlockKey];
-            return <Com data={item} key={id} index={index} />
-          })
-        }
+      <div className={cn(styles.content_body, "markdown-body")}>
+        {dataSource.map((item, index) => {
+          const { name, id } = item;
+          const Com = blockMap[name as BlockKey];
+          return <Com data={item} key={id} index={index} />;
+        })}
       </div>
     </div>
-  )
+  );
 };
 
 export default Content;
