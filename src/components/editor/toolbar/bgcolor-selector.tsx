722cf3d0ea50b9f872994dca9a5c20847aa7285f
NealST <m13710224694@163.com>
2025-01-29 18:49:41 +0800

    fix: add format for color selectors

--- src/components/editor/toolbar/bgcolor-selector.tsx
+++ src/components/editor/toolbar/bgcolor-selector.tsx
@@ -1,24 +1,45 @@
-import { useState } from 'react';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { Button } from '@/components/ui/button';
-import ColorPicker from './color-picker';
-import { Palette, ChevronsUpDown } from 'lucide-react';
-import type { IColorSelectorProps } from './types';
-import styles from './index.module.css';
+import { useState } from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import ColorPicker from "./color-picker";
+import { Palette, ChevronsUpDown } from "lucide-react";
+import { useFormatRange, processStyle } from "./controllers/use-format-range";
+import type { IBlockStateItem } from "../content/blocks/types";
+import type { IColorSelectorProps } from "./types";
+import styles from "./index.module.css";
 
-const BgColorSelector = function(props: IColorSelectorProps) {
-  const { disabled, initialColor = '' } = props;
+const BgColorSelector = function (props: IColorSelectorProps) {
+  const { disabled, initialColor = "" } = props;
   const [color, setColor] = useState(initialColor);
+  const formatRange = useFormatRange();
 
   function handleChange(newColor: string) {
     setColor(newColor);
+    formatRange((child: IBlockStateItem) => {
+      return {
+        ...child,
+        style: processStyle(
+          child.style || "",
+          `background-color:${newColor};`,
+          /background-color:(.*?);/
+        ),
+      };
+    });
   }
 
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button className={styles.tool_button} variant="ghost" disabled={disabled}>
-          <Palette size={16} />
+        <Button
+          className={styles.tool_button}
+          variant="ghost"
+          disabled={disabled}
+        >
+          <Palette size={16} color={color} />
           <ChevronsUpDown size={16} />
         </Button>
       </PopoverTrigger>
@@ -26,7 +47,7 @@ const BgColorSelector = function(props: IColorSelectorProps) {
         <ColorPicker value={color} onChange={handleChange} />
       </PopoverContent>
     </Popover>
-  )
+  );
 };
 
 export default BgColorSelector;
--- src/components/editor/toolbar/fontcolor-selector.tsx
+++ src/components/editor/toolbar/fontcolor-selector.tsx
@@ -3,14 +3,29 @@ import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover
 import { Button } from '@/components/ui/button';
 import ColorPicker from './color-picker';
 import { Baseline, ChevronsUpDown } from 'lucide-react';
+import { useFormatRange, processStyle } from "./controllers/use-format-range";
+import type { IBlockStateItem } from "../content/blocks/types";
 import type { IColorSelectorProps } from './types';
 import styles from './index.module.css';
 
 const FontColorSelector = function(props: IColorSelectorProps) {
   const { disabled, initialColor = '' } = props;
   const [color, setColor] = useState(initialColor);
+  const formatRange = useFormatRange();
+
   function handleChange(newColor: string) {
     setColor(newColor);
+    formatRange((child: IBlockStateItem) => {
+      return {
+        ...child,
+        style: processStyle(
+          child.style || "",
+          `;color:${newColor};`,
+          // to avoid the case that background-color matches this reg expression
+          /;color:(.*?);/
+        ),
+      };
+    });
   }
 
   return (
