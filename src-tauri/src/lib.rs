547f5b352ac0fe9d9f01a2d99cbbed70f5da941a
NealST <m13710224694@163.com>
2025-02-13 21:37:39 +0800

    feat: add settings menu in backend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,9 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 mod read_dir_recursive;
-
+use tauri::{
+  menu::{AboutMetadata, MenuBuilder, MenuItemBuilder, SubmenuBuilder},Manager
+};
+use tauri::Emitter;
 use read_dir_recursive::read_dir_recursive;
 
 #[tauri::command]
@@ -19,6 +22,50 @@ fn get_dir_info(path: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+          let handle = app.handle();
+
+          // my custom settings menu item
+          let settings = MenuItemBuilder::new("Setting")
+            .id("settings")
+            .accelerator("CmdOrCtrl+,")
+            .build(app)?;
+
+          // my custom app submenu
+          let app_submenu = SubmenuBuilder::new(app, "App")
+            .about(Some(AboutMetadata {
+              ..Default::default()
+            }))
+            .separator()
+            .item(&settings)
+            .separator()
+            .services()
+            .separator()
+            .quit()
+            .build()?;
+
+          // ... any other submenus
+          let menu = MenuBuilder::new(app)
+            .items(&[
+              &app_submenu,
+              // ... include references to any other submenus
+            ])
+            .build()?;
+
+          // set the menu
+          app.set_menu(menu)?;
+
+          // listen for menu item click events
+          app.on_menu_event(move |app, event| {
+            if event.id() == settings.id() {
+              // emit a window event to the frontend
+              println!("settings exec");
+              let _event = app.emit("custom-event", "/settings");
+            }
+          });
+
+          Ok(())
+        })
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
